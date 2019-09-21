dd85ef69c3ddea7c5f0023901d985ee5174ef012
春冨雅大 <masahiro@masanoMacBook-Pro.local>
2019-09-21 17:07:50 +0900

    fix index layout

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   def index
-    @messages = Message.all.order(created_at: "ASC")
+    @messages = Message.all.order(created_at: "DESC")
     @newMessage = Message.new
   end
 
